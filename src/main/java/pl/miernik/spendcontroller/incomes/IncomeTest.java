7fabbce0d82e50180ae3eaed1e47575e89ee4247
AdrianMiernik <adik.miernik@gmail.com>
2021-03-17 21:29:45 +0100

    bug fixing

--- src/main/java/pl/miernik/spendcontroller/incomes/IncomeTest.java
+++ src/main/java/pl/miernik/spendcontroller/incomes/IncomeTest.java
@@ -1,12 +1,28 @@
 package pl.miernik.spendcontroller.incomes;
 
 
-import lombok.*;
-
 import java.math.BigDecimal;
 
-@Value
+
 public class IncomeTest {
     private BigDecimal amount;
     private String name;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public IncomeTest setAmount(BigDecimal amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public IncomeTest setName(String name) {
+        this.name = name;
+        return this;
+    }
 }
